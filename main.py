77650ff42d355ff395e7bd783539ff289c72496e
Worthy Zhang <worthyzhang@gmail.com>
2017-04-23 23:06:00 +0800

    add radio api

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from flask import Flask
+from flask import render_template
+import os
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return render_template('home.html')
+
+
+@app.route('/radio/play')
+def radio_play():
+    os.system('mplayer http://bbcwssc.ic.llnwd.net/stream/bbcwssc_mp1_ws-einws')
+    print('radio play')
+
+@app.route('/radio/stop')
+def radio_stop():
+    os.system('mplayer q')
+    print('radio stop')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=80)
+    #app.run()
